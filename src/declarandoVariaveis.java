9a13693dfffd3f9484cc94a230556b763c4cf98b
EdgardMaia <edgard_maia@outlook.com>
2025-09-13 15:10:58 -0300

    feat: Adicionar programa para declaração de variáveis em Java

---
+++ src/declarandoVariaveis.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class declarandoVariaveis {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Digite seu nome: ");
+        String nome = sc.nextLine();
+        
+        System.out.print("Digite sua idade: ");
+        int idade = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Digite sua altura em metros: ");
+        float altura = sc.nextFloat();
+        sc.nextLine();
+
+        System.out.print("É estudante? (S/N):");
+        String resposta = sc.nextLine();
+        String estudante;
+
+        if (resposta.equalsIgnoreCase("S")) {
+            estudante = "Você é estudante.";
+        } else {
+            estudante = "Você não é estudante.";
+        }
+
+        System.out.printf("Nome: %s\nIdade: %d\nAltura: %.2f metros\n%s\n", nome, idade, altura, estudante);
+    }
+}
