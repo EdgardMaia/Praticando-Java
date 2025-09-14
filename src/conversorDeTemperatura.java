3511bbc7df351b85bc091b1aa07bce2d71ef3d32
EdgardMaia <edgard_maia@outlook.com>
2025-09-14 10:51:03 -0300

    refactor: Mover exercícios para pasta Variaveis-e-Tipos
    Remove arquivos da pasta src/ raiz para reorganizar em:
    src/Variaveis-e-Tipos/
    
    Exercícios movidos:
    - cadastroDeLivros.java
    - classificacaoDeProduto.java
    - consumoAutonomia.java
    - conversaoDeTipos.java
    - conversorDeDolares.java
    - conversorDeTemperatura.java
    - declarandoVariaveis.java
    - media.java
    - parOuImpar.java
    - verificacaoIdade.java
    
    Melhora a organização do projeto por tópicos de estudo.

--- src/conversorDeTemperatura.java
+++
@@ -1,7 +0,0 @@
-public class conversorDeTemperatura {
-    public static void main(String[] args) {
-        int Celsius = 20;
-        double Fahrenheit = (Celsius * 9 / 5) + 32;
-        System.out.printf("Temperatura em Fahrenheit: %.2f\n", Fahrenheit);
-    }
-}
